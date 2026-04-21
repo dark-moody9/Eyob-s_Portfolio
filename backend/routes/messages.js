e2afd0ea698c2f6c957bbb641e470e27c8afe4de
Eyob <eyobabebe1222@gmail.com>
2026-04-21 16:47:10 +0200

    remove backend

--- backend/routes/messages.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { authenticate } = require('../middleware/authMiddleware');
-const { getMessages } = require('../controllers/messageController');
-
-router.get('/', authenticate, getMessages);  // Only logged-in users can access
-
-module.exports = router;
